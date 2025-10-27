6247f27086ecc2ddccf864797d69754684af5d28
Nicole Gu <nguob@uw.edu>
2025-10-26 17:14:03 -0700

    Added better logging and fixed PR creation. Hopefully fixed generation error when response isn't 'yes' or 'no'.

--- scripts/label_extraction.py
+++ scripts/label_extraction.py
@@ -37,11 +37,9 @@ MLFLOW_TRACKING_URI = "http://localhost:5000"
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 pretty.install()
-console = get_console()
+console: Console = get_console()
 logger = get_logger(
-    "".join(["scripts/", __file__.split("/")[-1].split(".")[0], f".main"]),
-    # console=console,
-    level="DEBUG",
+    "".join(["chexagent.scripts.", __file__.split("/")[-1].split(".")[0], f".main"])
 )
 # get_logger("mlflow").setLevel("DEBUG")
 
@@ -101,7 +99,7 @@ def log_results(data: DatasetDict, args: DictConfig):
 
 @hydra.main(config_path="../configs", config_name="chexagent", version_base="1.1")
 def main(args: DictConfig):
-    intro(args)
+    intro(args, "CheXagent Label Extraction")
     load_dotenv(args.get("env_file", None))
     name = args.get("name", "label-extraction")
     prefix = args.get("prefix", "-test") if args.dry_run else ""
@@ -134,11 +132,16 @@ def main(args: DictConfig):
             lambda x: x["dicom_id"] in gt_val_dicom_ids
         )
         data["test"] = (
-            data["test"].shuffle(seed=42).select(range(len(gt_val_dicom_ids)))
+            data["test"].shuffle(seed=42).select(range(len(gt_val_dicom_ids) // 2))
         )
 
     console.print(data)
     logger.info(f"Dataset loaded. Loading '{args.model_id}' model")
+
+    # Silence attention mask warning
+    import transformers
+
+    transformers.logging.set_verbosity_error()
     model = CheXagent(model_name=args.model_id, dtype=torch.float32)
     yes_no = {"yes": 1, "no": 0}
     model_name: str = args.model_id.split("/")[-1]
@@ -154,18 +157,27 @@ def main(args: DictConfig):
             prompt = textwrap.dedent(
                 f"""
                 Consider the following chest X-ray description:
-                
-                ```
+
                 {text}
-                ```
 
                 Does this chest X-ray contain a {col}?
                 """
-            )
+            ).strip()
             response = model.generate([image_file], prompt).lower().strip()
-            y_pred = yes_no[response]
+            debug_info = {
+                "prompt": prompt,
+                "response": response,
+                "dicom_id": example["dicom_id"],
+            }
+            logger.debug(debug_info)
             new_col = f"{col}-{model_name}"
-            example[new_col] = y_pred
+            try:
+                y_pred = yes_no[response]
+                example[new_col] = y_pred
+            except KeyError as e:
+                logger.warn(f"Invalid response returned. See: {debug_info}")
+                example[new_col] = -9
+
         return example
 
     # logger.info(f"Creating {args.model} labels....")
@@ -182,16 +194,19 @@ def main(args: DictConfig):
     ) as run:
         mlflow.autolog()
         log_params(args)
-        with torch.inference_mode():
-            logger.info(f"Get binary labels for {args.target_columns}")
-            labelled_ds = data.map(
-                get_response_with_report,
-                batched=args.get("batched", False),
-                batch_size=args.batch_size,
-                with_rank=with_rank,
-                num_proc=num_proc,
-                desc="In progress",
-            )
+        with console.status(
+            f"\t[bold]Getting binary labels for [green]'{args.target_columns}'[/green][/bold]\n",
+            spinner="arrow3",
+        ):
+            with torch.inference_mode():
+                labelled_ds = data.map(
+                    get_response_with_report,
+                    batched=args.get("batched", False),
+                    batch_size=args.batch_size,
+                    with_rank=with_rank,
+                    num_proc=num_proc,
+                    desc="In progress",
+                )
 
         logger.info(f"Inference complete. Logging results to mlflow")
         log_results(labelled_ds, args)
